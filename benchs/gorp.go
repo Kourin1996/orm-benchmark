0ab63eac4f83467f19ea11692f9e7740a7b4ce13
kourin <kourin1996@gmail.com>
2021-02-26 15:34:32 +0900

    add benchs for gorp

--- benchs/gorp.go
+++ benchs/gorp.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	"database/sql"
+
 	_ "github.com/lib/pq"
 	"gopkg.in/gorp.v1"
 )
@@ -22,20 +23,29 @@ func init() {
 		db, err := sql.Open("postgres", ORM_SOURCE)
 		checkErr(err)
 		d := &gorp.DbMap{Db: db, Dialect: gorp.PostgresDialect{}}
+		d.AddTableWithName(Model{}, "models").SetKeys(true, "Id")
 		dbmap = d
 	}
+}
+
+func initGorpDB() {
+	err := dbmap.DropTableIfExists(Model{})
+	checkErr(err)
+
+	err = dbmap.CreateTablesIfNotExists()
+	checkErr(err)
 }
 
 func GorpInsert(b *B) {
 	var m *Model
 	wrapExecute(b, func() {
-		initDB()
+		initGorpDB()
 		m = NewModel()
 	})
 	for i := 0; i < b.N; i++ {
 		m.Id = 0
-		d := dbmap.Insert(&m)
-		if d.Error != nil {
+		d := dbmap.Insert(m)
+		if d != nil {
 			fmt.Println(d.Error())
 			b.FailNow()
 		}
@@ -43,17 +53,75 @@ func GorpInsert(b *B) {
 }
 
 func GorpInsertMulti(b *B) {
-	panic(fmt.Errorf("in preparation"))
+	panic(fmt.Errorf("Not support multi insert"))
 }
 
 func GorpUpdate(b *B) {
-	panic(fmt.Errorf("in preparation"))
+	var m *Model
+	wrapExecute(b, (func() {
+		initGorpDB()
+		m = NewModel()
+
+		d := dbmap.Insert(m)
+		if d != nil {
+			fmt.Println(d.Error())
+			b.FailNow()
+		}
+	}))
+
+	for i := 0; i < b.N; i++ {
+		_, d := dbmap.Update(m)
+		if d != nil {
+			fmt.Println(d.Error())
+			b.FailNow()
+		}
+	}
 }
 
 func GorpRead(b *B) {
-	panic(fmt.Errorf("in preparation"))
+	var m *Model
+	wrapExecute(b, (func() {
+		initGorpDB()
+		m = NewModel()
+
+		d := dbmap.Insert(m)
+		if d != nil {
+			fmt.Println(d.Error())
+			b.FailNow()
+		}
+	}))
+
+	for i := 0; i < b.N; i++ {
+		d := dbmap.SelectOne(m, "select * from models where id = $1", m.Id)
+		if d != nil {
+			fmt.Println(d.Error())
+			b.FailNow()
+		}
+	}
 }
 
 func GorpReadSlice(b *B) {
-	panic(fmt.Errorf("in preparation"))
+	var m *Model
+	wrapExecute(b, (func() {
+		initGorpDB()
+		m = NewModel()
+
+		for i := 0; i < 100; i++ {
+			m.Id = 0
+			d := dbmap.Insert(m)
+			if d != nil {
+				fmt.Println(d.Error())
+				b.FailNow()
+			}
+		}
+	}))
+
+	for i := 0; i < b.N; i++ {
+		var models []Model
+		_, d := dbmap.Select(&models, "select * from models where id > $1 limit 100", m.Id)
+		if d != nil {
+			fmt.Println(d.Error())
+			b.FailNow()
+		}
+	}
 }
